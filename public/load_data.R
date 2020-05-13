9d0ea8aae03c984dd56bedf1540c94a0093b13c3
mikelove <michaelisaiahlove@gmail.com>
2020-05-13 17:38:47 -0400

    loading data script

---
+++ public/load_data.R
@@ -0,0 +1,76 @@
+dir <- file.path("Artery_Tibial","PHACTR1")
+cond.files <- sub(".tsv","",list.files(dir, ".tsv"))
+ncond <- length(cond.files)
+ld.mat <- list()
+sum.stats <- list()
+for (j in 1:ncond) {
+  filename <- file.path(dir,paste0(cond.files[j], ".ld"))
+  ld.mat[[j]] <- as.matrix(read.delim(filename,header=FALSE))
+  filename <- file.path(dir,paste0(cond.files[j], ".tsv"))
+  sum.stats[[j]] <- read.delim(filename)
+  sum.stats[[j]] <- sum.stats[[j]][order(sum.stats[[j]]$pos),]
+}
+
+sapply(sum.stats, nrow)
+# 74 113 157
+# 74 75 12
+
+nsnp <- 50
+ncond <- 3
+Sigma_a <- array(NA, dim=c(nsnp,nsnp,ncond))
+beta_hat_a <- matrix(NA, nrow=nsnp, ncol=ncond)
+beta_hat_b <- matrix(NA, nrow=nsnp, ncol=ncond)
+se_a <- matrix(NA, nrow=nsnp, ncol=ncond)
+se_b <- matrix(NA, nrow=nsnp, ncol=ncond)
+indices <- list(25:74, 51:100, 1:50)
+for (j in 1:3) {
+  jj <- j + 1
+  idx <- with(sum.stats[[jj]], which.max(abs(beta_eQTL/se_eQTL)))
+  print(idx)
+  with(sum.stats[[jj]], stopifnot(Ref_GWAS == Ref_eQTL | Ref_GWAS == Effect_eQTL))
+  with(sum.stats[[jj]], table(Ref_GWAS == Ref_eQTL))
+  flip <- with(sum.stats[[jj]], which(Ref_GWAS != Ref_eQTL))
+  # flip GWAS so that same allele is described for GWAS as for eQTL
+  sum.stats[[jj]]$beta_GWAS_flipped <- sum.stats[[jj]]$beta_GWAS
+  sum.stats[[jj]]$beta_GWAS_flipped[flip] <- -1 * sum.stats[[jj]]$beta_GWAS[flip]
+  ld.sign <- sign(ld.mat[[jj]][,idx])
+  beta_a <- with(sum.stats[[jj]], ifelse(Major_plink == Ref_eQTL, 1, -1) * ld.sign * beta_eQTL)
+  beta_b <- with(sum.stats[[jj]], ifelse(Major_plink == Ref_eQTL, 1, -1) * ld.sign * beta_GWAS_flipped)
+  # flip signs so that effect size is in terms of expression increasing allele in eQTL
+  if (beta_a[idx] < 0) {
+    beta_a <- beta_a * -1
+    beta_b <- beta_b * -1
+  }
+  #plot(beta_a, beta_b)
+  beta_hat_a[,j] <- beta_a[indices[[j]]]
+  beta_hat_b[,j] <- beta_b[indices[[j]]]
+  se_a[,j] <- sum.stats[[jj]]$se_eQTL[indices[[j]]]
+  se_b[,j] <- sum.stats[[jj]]$se_GWAS[indices[[j]]]
+  # flip LD matrix so that alleles are positively correlated with eQTL index
+  ld.sign <- sign(ld.mat[[jj]][,idx])
+  ld.flipped <- t(t(ld.mat[[jj]]) * ld.sign) * ld.sign
+  Sigma_a[,,j] <- ld.flipped[indices[[j]],indices[[j]]]
+}
+
+plot(beta_hat_a, beta_hat_b, col=rep(1:3,each=50))
+
+library(Matrix)
+Sigma_np <- Sigma_a
+for (j in 1:3) {
+  Sigma_np[,,j] <- as.matrix(nearPD(Sigma_a[,,j])$mat)
+}
+pheatmap::pheatmap(Sigma_a[,,1], cluster_rows=FALSE, cluster_cols=FALSE)
+pheatmap::pheatmap(Sigma_np[,,1], cluster_rows=FALSE, cluster_cols=FALSE)
+Sigma_b <- Sigma_a <- Sigma_np
+
+data <- list(nsnp=nsnp,
+             ncond=ncond,
+             beta_hat_a=beta_hat_a,
+             beta_hat_b=beta_hat_b,
+             se_a=se_a,
+             se_b=se_b,
+             Sigma_a=Sigma_a,
+             Sigma_b=Sigma_b)
+load_all("../../mrlocus")
+options(mc.cores=2)
+fit1 <- fitBetaEcaviar(data)
