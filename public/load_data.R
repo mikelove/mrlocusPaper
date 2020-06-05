f55da9ed42b3a159d0b0eb3e7ca705a0f7c85852
mikelove <michaelisaiahlove@gmail.com>
2020-06-05 16:45:46 -0400

    move to new code base

--- public/load_data.R
+++ public/load_data.R
@@ -15,8 +15,6 @@ ld.mat <- ld.mat[2:4]
 sum.stats <- sum.stats[2:4]
 
 sapply(sum.stats, nrow)
-# 74 113 157
-# 74 75 12
 
 library(pheatmap)
 for (j in 1:3) {
@@ -33,16 +31,13 @@ for (j in 1:3) {
 
 pheatmap(ld.mat[[3]])
 
-sapply(sum.stats, nrow)
-
-nsnp <- 50
+nsnp <- sapply(sum.stats, nrow)
 ncond <- 3
-Sigma_a <- array(NA, dim=c(nsnp,nsnp,ncond))
-beta_hat_a <- matrix(NA, nrow=nsnp, ncol=ncond)
-beta_hat_b <- matrix(NA, nrow=nsnp, ncol=ncond)
-se_a <- matrix(NA, nrow=nsnp, ncol=ncond)
-se_b <- matrix(NA, nrow=nsnp, ncol=ncond)
-indices <- list(25:74, 51:100, 1:50)
+Sigma_a <- list()
+beta_hat_a <- list()
+beta_hat_b <- list()
+se_a <- list()
+se_b <- list()
 for (j in 1:3) {
   idx <- with(sum.stats[[j]], which.max(abs(beta_eQTL/se_eQTL)))
   print(idx)
@@ -61,35 +56,47 @@ for (j in 1:3) {
     beta_b <- beta_b * -1
   }
   #plot(beta_a, beta_b)
-  beta_hat_a[,j] <- beta_a[indices[[j]]]
-  beta_hat_b[,j] <- beta_b[indices[[j]]]
-  se_a[,j] <- sum.stats[[j]]$se_eQTL[indices[[j]]]
-  se_b[,j] <- sum.stats[[j]]$se_GWAS[indices[[j]]]
+  beta_hat_a[[j]] <- beta_a
+  beta_hat_b[[j]] <- beta_b
+  se_a[[j]] <- sum.stats[[j]]$se_eQTL
+  se_b[[j]] <- sum.stats[[j]]$se_GWAS
   # flip LD matrix so that alleles are positively correlated with eQTL index
   ld.sign <- sign(ld.mat[[j]][,idx])
   ld.flipped <- t(t(ld.mat[[j]]) * ld.sign) * ld.sign
-  Sigma_a[,,j] <- ld.flipped[indices[[j]],indices[[j]]]
+  Sigma_a[[j]] <- ld.flipped
 }
 
-plot(beta_hat_a, beta_hat_b, col=rep(1:3,each=50))
+plot(unlist(beta_hat_a), unlist(beta_hat_b), col=rep(1:3,nsnp))
 
 library(Matrix)
 Sigma_np <- Sigma_a
 for (j in 1:3) {
-  Sigma_np[,,j] <- as.matrix(nearPD(Sigma_a[,,j])$mat)
+  Sigma_np[[j]] <- as.matrix(nearPD(Sigma_a[[j]])$mat)
 }
-pheatmap::pheatmap(Sigma_a[,,1], cluster_rows=FALSE, cluster_cols=FALSE)
-pheatmap::pheatmap(Sigma_np[,,1], cluster_rows=FALSE, cluster_cols=FALSE)
+
+pheatmap::pheatmap(Sigma_a[[1]], cluster_rows=FALSE, cluster_cols=FALSE)
+pheatmap::pheatmap(Sigma_np[[1]], cluster_rows=FALSE, cluster_cols=FALSE)
 Sigma_b <- Sigma_a <- Sigma_np
 
-data <- list(nsnp=nsnp,
-             ncond=ncond,
-             beta_hat_a=beta_hat_a,
-             beta_hat_b=beta_hat_b,
-             se_a=se_a,
-             se_b=se_b,
-             Sigma_a=Sigma_a,
-             Sigma_b=Sigma_b)
 load_all("../../mrlocus")
 options(mc.cores=2)
-fit1 <- fitBetaEcaviar(data)
+j <- 3
+fit1 <- fitBetaEcaviar(nsnp=nsnp[j],
+                       beta_hat_a=beta_hat_a[[j]],
+                       beta_hat_b=beta_hat_b[[j]],
+                       se_a=se_a[[j]],
+                       se_b=se_b[[j]],
+                       Sigma_a=Sigma_a[[j]],
+                       Sigma_b=Sigma_b[[j]])
+
+rstan::stan_plot(fit1, pars=paste0("beta_a[",1:nsnp[j],"]"))
+rstan::stan_plot(fit1, pars=paste0("beta_b[",1:nsnp[j],"]"))
+print(fit1, pars=paste0("beta_a[",1:nsnp[3],"]"), digits=3)
+coefs1 <- rstan::extract(fit1)
+par(mfrow=c(1,2))
+beta_clean_a <- colMeans(coefs1$beta_a)
+plot(beta_hat_a[[j]], beta_clean_a, asp=1)
+abline(0,1); abline(h=0, lty=2)
+beta_clean_b <- colMeans(coefs1$beta_b)
+plot(beta_hat_b[[j]], beta_clean_b, asp=1)
+abline(0,1); abline(h=0, lty=2)
