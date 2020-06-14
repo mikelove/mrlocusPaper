da1a08da10ac4475d6dc84d89ed18d2d78f0397d
mikelove <michaelisaiahlove@gmail.com>
2020-06-14 18:15:08 -0400

    clean code and move to pkg

--- public/load_data.R
+++ public/load_data.R
@@ -23,51 +23,21 @@ out <- collapseHighCorSNPs(sum_stat, ld_mat)
 sum_stat <- out$sum_stat
 ld_mat <- out$ld_mat
 
-flipAlleles <- function(sum_stat, ld_mat) {
-  Sigma <- list()
-  beta_hat_a <- list()
-  beta_hat_b <- list()
-  se_a <- list()
-  se_b <- list()
-  for (j in seq_along(sum_stat)) {
-    # the index SNP in the signal cluster for A (eQTL)
-    idx <- with(sum_stat[[j]], which.max(abs(beta_eQTL/se_eQTL)))
-    # reference B (GWAS) allele must be either reference or effect allele in A (eQTL)
-    with(sum_stat[[j]], stopifnot(Ref_GWAS == Ref_eQTL | Ref_GWAS == Effect_eQTL))
-    # flip B (GWAS) so that same allele is described for B (GWAS) as for A (eQTL)
-    flip <- with(sum_stat[[j]], which(Ref_GWAS != Ref_eQTL))
-    sum_stat[[j]]$beta_GWAS_flipped <- sum_stat[[j]]$beta_GWAS
-    sum_stat[[j]]$beta_GWAS_flipped[flip] <- -1 * sum_stat[[j]]$beta_GWAS[flip]
-    # flip alleles other than the index so they have positive correlation (LD) with index
-    ld.sign <- sign(ld_mat[[j]][,idx])
-    beta_a <- with(sum_stat[[j]], ifelse(Major_plink == Ref_eQTL, 1, -1) * ld.sign * beta_eQTL)
-    beta_b <- with(sum_stat[[j]], ifelse(Major_plink == Ref_eQTL, 1, -1) * ld.sign * beta_GWAS_flipped)
-    ld.flipped <- t(t(ld_mat[[j]]) * ld.sign) * ld.sign
-    Sigma[[j]] <- ld.flipped
-    # flip alleles so that effect size is positive for index SNP for A (eQTL)
-    if (beta_a[idx] < 0) {
-      beta_a <- beta_a * -1
-      beta_b <- beta_b * -1
-    }
-    beta_hat_a[[j]] <- beta_a
-    beta_hat_b[[j]] <- beta_b
-    se_a[[j]] <- sum_stat[[j]]$se_eQTL
-    se_b[[j]] <- sum_stat[[j]]$se_GWAS
-  }
-}
-
 nsnp <- sapply(sum_stat, nrow)
-plot(unlist(beta_hat_a), unlist(beta_hat_b), col=rep(1:3,nsnp))
+out <- flipAllelesAndGather(sum_stat, ld_mat,
+                            a="eQTL", b="GWAS",
+                            ref="Ref", eff="Effect",
+                            beta="beta", se="se",
+                            major_plink="Major_plink",
+                            sep="_", ab_last=TRUE)
+plot(unlist(out$beta_hat_a), unlist(out$beta_hat_b), col=rep(1:3,nsnp))
 
 library(Matrix)
-Sigma_np <- Sigma_a
+Sigma_np <- out$Sigma
 for (j in 1:3) {
-  Sigma_np[[j]] <- as.matrix(nearPD(Sigma_a[[j]])$mat)
+  Sigma_npd[[j]] <- as.matrix(nearPD(Sigma_a[[j]])$mat)
 }
-
-pheatmap::pheatmap(Sigma_a[[1]], cluster_rows=FALSE, cluster_cols=FALSE)
-pheatmap::pheatmap(Sigma_np[[1]], cluster_rows=FALSE, cluster_cols=FALSE)
-Sigma_b <- Sigma_a <- Sigma_np
+Sigma_b <- Sigma_a <- Sigma_npd
 
 load_all("../../mrlocus")
 options(mc.cores=2)
