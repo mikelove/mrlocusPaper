490ef675008f012dcc415594a035b08210a1928a
mikelove <michaelisaiahlove@gmail.com>
2020-06-29 17:27:28 -0400

    plot work

--- sim/final_plot.R
+++ sim/final_plot.R
@@ -1,6 +1,6 @@
 files <- sub(".final","",list.files("out", pattern="final"))
+files <- grep("^3_",files,value=TRUE)
 files
-
 final <- list()
 for (i in seq_along(files)) {
   final[[i]] <- read.table(paste0("out/",files[i],".final"),skip=1)
@@ -19,6 +19,8 @@ tab <- dat %>% group_by(method) %>%
             RMSE=sqrt(mean((est_nozero-true)^2,na.rm=TRUE)))
 tab
 data.tb <- tibble(x=-.7, y=.7, tb=list(tab))
+data.tb <- tibble(x=-.9, y=.9, tb=list(tab))
+data.tb <- tibble(x=-.4, y=.4, tb=list(tab))
 
 library(ggplot2)
 library(ggpmisc)
@@ -27,7 +29,12 @@ ggplot(dat, aes(true,estimate,col=method)) +
   geom_point() +
   geom_abline(intercept=0, slope=1) +
   scale_color_manual(values=cols) +
-  xlim(-.75,.75) + ylim(-.75,.75) +
   geom_table(data=data.tb, aes(x, y, label=tb),
              table.theme = ttheme_gtlight,
-             stat="fmt_tb")
+             stat="fmt_tb") +
+#  xlim(-.75,.75) + ylim(-.75,.75) + 
+#  ggtitle("Sim 1: 10% h2g, 1% var. exp.")
+#  xlim(-1,1) + ylim(-1,1) + 
+#  ggtitle("Sim 2: 10% h2g, 2% var. exp.")
+  xlim(-.5,.5) + ylim(-.5,.5) + 
+  ggtitle("Sim 3: 20% h2g, 1% var. exp.")
