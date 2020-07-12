4bca5577338977c3ca197b6f6eb7ffbd7073d02e
mikelove <michaelisaiahlove@gmail.com>
2020-07-12 08:21:47 -0400

    working on sim plots

--- sim/sim_review.R
+++ sim/sim_review.R
@@ -70,24 +70,63 @@ dap <- lapply(1:12, function(i) {
   })
 })
 names(dap) <- c(1:9,paste0("null",1:3))
-save(i2, causal, clumps, dap, file="sim_review.rda")
+#save(i2, causal, clumps, dap, file="sim_review.rda")
+
+load("sim_review.rda")
+
+.1 .01 - 1
+.05 .01 - 2 
+.2 .01 - 3
+.1 .005 - 4 
+.1 .001 - 5 
+.2 .005 - 6 
+.2 .001 - 7 
+.05 .005 - 8 
+.05 .001 - 9
+
+h2 <- rep(c(.1,.2,.05),4)
+ve <- c(rep(c(.01,.005,.001),each=3),c(0,0,0))
+sim <- c(LETTERS[1:9], paste0("Null-",c(.1,.2,.05)))
+key <- data.frame(value=c(h2,ve),
+                  sim=factor(rep(sim,2),levels=sim),
+                  id=rep(c(1,3,2, 4,6,8, 5,7,9, "null1","null2","null3"), 2),
+                  type=rep(c("eQTL h2","variance explained"),each=12),
+                  group=factor(rep(rep(1:4,each=3),2)))
 
 library(ggplot2)
+cols <- palette.colors(4, palette="Set 2")
+ggplot(key, aes(sim, value, fill=group)) +
+  geom_bar(stat="identity", show.legend=FALSE) +
+  facet_wrap(~type, nrow=2, scales="free") +
+  scale_fill_manual(values=cols)
+
+key2 <- key[1:12,c("sim","id","group")]
+
 library(ggbeeswarm)
 plotit <- function(x, dot=TRUE, bee=FALSE) {
   dat <- data.frame(number=unlist(x),
-                    sim=rep(names(x), lengths(x)))
-  g <- ggplot(dat, aes(sim, number)) +
-    geom_violin() +
-    theme_bw()
+                    id=rep(names(x), lengths(x)))
+
+  idx <- match(dat$id, key2$id)
+  dat$sim <- key2$sim[idx]
+  dat$group <- key2$group[idx]
+  dat$sim <- factor(dat$sim, levels=key2$sim)
+
+  g <- ggplot(dat, aes(sim, number, fill=group)) +
+    geom_violin(show.legend=FALSE) +
+    theme_bw() +
+    scale_fill_manual(values=cols)
+  
   if (dot) {
     g <- g + geom_dotplot(binaxis="y",
                           stackdir="center",
                           dotsize=.5,
-                          stackratio=1.25)
+                          stackratio=1.25,
+                          fill="black",
+                          show.legend=FALSE)
   }
   if (bee) {
-    g <- g + geom_quasirandom()
+    g <- g + geom_quasirandom(show.legend=FALSE)
   }
   g
 }
