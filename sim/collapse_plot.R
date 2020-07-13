4b5ba4411c5d4b95b2e9b3c81910ce5ca789ba5c
mikelove <michaelisaiahlove@gmail.com>
2020-07-13 11:28:16 -0400

    another plot

---
+++ sim/collapse_plot.R
@@ -0,0 +1,12 @@
+pre <- as.numeric(scan("collapse_pre.txt", what="char"))
+post <- as.numeric(scan("collapse_post.txt", what="char"))
+
+dat <- data.frame(type=factor(rep(c("pre","post"),each=length(pre)),c("pre","post")),
+                  number=c(pre,post))
+
+library(ggplot2)
+ggplot(dat, aes(x=number, fill=type)) +
+  geom_histogram(position="identity",
+                 alpha=.25, color="grey50",
+                 breaks=0:30*5) +
+  theme_bw() + xlab("number of SNPs per clump")
