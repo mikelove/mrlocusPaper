3d0cb653685796d5c1af266f204075b6c2653758
mikelove <michaelisaiahlove@gmail.com>
2020-08-10 12:03:42 -0400

    adding forest plot

--- public/final_plot.R
+++ public/final_plot.R
@@ -5,19 +5,18 @@ q <- qnorm(.9)
 dat <- dat %>% mutate(xmin=alpha-q*se,xmax=alpha+q*se)
 
 # replace SD based intervals with quantile based
-ints <- c(0.04757, 0.13474,
-          -0.3938, -0.2832,
-          0.02920, 0.06893,
-          -0.9084,  0.0652,
-          -0.15772,  0.04035,
-          -0.04712, -0.04113)
-ints <- matrix(ints, ncol=2, byrow=TRUE)
-dat[13:18,6:7] <- ints
+mrlocusFiles <- list.files(pattern=".txt")
+ints <- lapply(mrlocusFiles, function(f) read.table(f, header=TRUE)[1,3:4])
+ints <- do.call(rbind, ints)
+dat[dat$method == "mrlocus",c("xmin","xmax")] <- ints
 
-png(file="~/Desktop/realdata.png", res=125, width=1200, height=600)
+dat$label <- paste0(dat$gene," (",dat$tissue,")", " → ", dat$trait)
+dat$label <- factor(dat$label, levels=dat$label[1:5])
+
+png(file="../supp/figs/forest.png", width=1200, height=600, res=150)
 ggplot(dat, aes(x=alpha,y=method,xmin=xmin,xmax=xmax)) +
   geom_pointrange() +
-  facet_wrap(~tissue * gene, scales="free") +
+  facet_wrap(~label, scales="free") +
   geom_vline(xintercept = 0, lty=2) +
   xlab("gene-to-trait estimate")
 dev.off()
