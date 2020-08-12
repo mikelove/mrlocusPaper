0e6b592a8d28dc74a125dccb6a7528d5663dad55
mikelove <michaelisaiahlove@gmail.com>
2020-08-12 15:23:29 -0400

    use TwoSampleMR

--- sim/collate.R
+++ sim/collate.R
@@ -6,15 +6,19 @@ ptwas.filename <- cmd_args[3]
 mrlocus.filename <- cmd_args[4]
 out.filename <- cmd_args[5]
 
+ivw.fixed.effects <- function(eqtl.beta, gwas.beta, eqtl.se, gwas.se) {
+  res <- TwoSampleMR::mr_ivw_fe(eqtl.beta, gwas.beta, eqtl.se, gwas.se)
+  c("Estimate"=res$b, "Std. Error"=res$se)
+}
+
 estimate <- function(filename) {
   x <- read.delim(filename)
-  #with(x, plot(gwas.true ~ eqtl.true))
   mycoef <- function(fit) suppressWarnings(summary(fit)$coefficients[1,1:2])
   out <- cbind(true=mycoef(lm(x$gwas.true ~ x$eqtl.true + 0)),
                caus=with(x[x$eqtl.true != 0,],mycoef(lm(gwas.beta ~ eqtl.beta + 0))),
-               caus.wt=with(x[x$eqtl.true != 0,],mycoef(lm(gwas.beta ~ eqtl.beta + 0, weight=1/eqtl.se^2))),
+               caus.wt=with(x[x$eqtl.true != 0,],ivw.fixed.effects(eqtl.beta, gwas.beta, eqtl.se, gwas.se)),
                all=mycoef(lm(x$gwas.beta ~ x$eqtl.beta + 0)),
-               all.wt=mycoef(lm(x$gwas.beta ~ x$eqtl.beta + 0, weight=1/x$eqtl.se^2)))
+               all.wt=with(x, ivw.fixed.effects(eqtl.beta, gwas.beta, eqtl.se, gwas.se)))
   out[2,1] <- NA
   out
 }
