3b269dff73080d30c633baf624621e1e98d81711
mikelove <michaelisaiahlove@gmail.com>
2020-11-19 13:02:34 -0500

    adding more sim

--- corr_instr_sim/corr_instr_sim.R
+++ corr_instr_sim/corr_instr_sim.R
@@ -1,16 +1,24 @@
-n <- 8
-r2 <- .9
-sd <- .1
-corr <- sqrt(r2)
-Sigma <- matrix(corr, nrow=n, ncol=n)
-diag(Sigma) <- 1
-beta_hat <- MASS::mvrnorm(2, mu=rep(0,n), Sigma)
-rownames(beta_hat) <- c("a","b")
-beta_hat["b",] <- sign(beta_hat["a",]) * beta_hat["b",]
-beta_hat["a",] <- abs(beta_hat["a",])
-sd_b <- sd_a <- rep(sd,n)
-#devtools::load_all("../../mrlocus")
-res <- list(beta_hat_a=beta_hat["a",], sd_a=sd_a, beta_hat_b=beta_hat["b",], sd_b=sd_b)
-suppressWarnings({ fit <- fitSlope(res, verbose=FALSE, open_progress=FALSE, show_messages=FALSE, refresh=-1) })
-rstan::summary(fit$stanfit, pars="alpha", probs=c(.1,.9))$summary
-plotMrlocus(fit)
+devtools::load_all("../../mrlocus")
+library(pbapply)
+
+niter <- 100
+out <- pbsapply(1:niter, function(i) {
+  n <- 6
+  r2 <- 0.9
+  sdev <- .5
+  corr <- sqrt(r2)
+  Sigma <- matrix(corr, nrow=n, ncol=n)
+  diag(Sigma) <- 1
+  beta_hat_a <- MASS::mvrnorm(1, mu=rep(5,n), Sigma)  
+  beta_hat_b <- MASS::mvrnorm(1, mu=rep(0,n), Sigma)
+  sd_b <- sd_a <- rep(sdev,n)
+  res <- list(beta_hat_a=beta_hat_a, sd_a=sd_a, beta_hat_b=beta_hat_b, sd_b=sd_b)
+  co <- capture.output({
+    suppressWarnings({ 
+      fit <- fitSlope(res, verbose=FALSE, open_progress=FALSE, show_messages=FALSE, refresh=-1)
+    })
+  })
+  bnds <- rstan::summary(fit$stanfit, pars="alpha", probs=c(.1,.9))$summary[1,c("10%","90%")]
+  unname(bnds)
+})
+mean(apply(out, 2, function(x) sign(x[1]) == sign(x[2])))
