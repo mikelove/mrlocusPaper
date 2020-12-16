b45f0a56671ac926a1cdb48ca02c40266638d09f
mikelove <michaelisaiahlove@gmail.com>
2020-12-16 08:20:34 -0500

    update assessment code

--- sim/assess_coloc.R
+++ sim/assess_coloc.R
@@ -3,23 +3,16 @@ dir <- file.path("out",type)
 files <- list.files(dir, pattern="ve.clumped")
 
 library(pbapply)
-out <- pblapply(1:20, function(i) {
+out <- pblapply(seq_along(files), function(i) {
   file <- sub(".clumped","",files[i])
   clumped.filename <- paste0(dir,"/",file,".clumped")
   scan.filename <- paste0(dir,"/",file,".scan.tsv")
-  ld.filename <- paste0(dir,"/",file,".ld")
   clumped <- read.table(clumped.filename, strings=FALSE, header=TRUE)
   big_sum_stat <- read.delim(scan.filename, strings=FALSE)
-  big_ld_mat <- as.matrix(read.table(ld.filename))
   noparen <- function(z) sub("\\(1\\)","",z)
   clumps <- lapply(strsplit(clumped$SP2,split=","), noparen)
   # add index SNP to clumps
   clumps <- lapply(seq_along(clumps), function(j) c(clumped$SNP[j], clumps[[j]]))
-  ld_mat <- lapply(clumps, function(x) {
-    idx <- big_sum_stat$snp %in% x
-    unname(big_ld_mat[idx,idx,drop=FALSE])
-  })
-  sapply(ld_mat, nrow)
   sum_stat <- lapply(clumps, function(x) {
     out <- big_sum_stat[big_sum_stat$snp %in% x,]
     out$z <- out$eqtl.beta/out$eqtl.se
@@ -27,8 +20,15 @@ out <- pblapply(1:20, function(i) {
     out$z2 <- out$gwas.beta/out$gwas.se # for testing ecaviar
     out
   })
-  load(paste0(dir,"/",file,".mrl_coloc")) # res
-  load(paste0(dir,"/",file,".ecav")) # ecav.coloc
+  # load coloc results
+  load(paste0(dir,"/",file,".mrl_coloc")) # MRLocus coloc results - `res`
+  load(paste0(dir,"/",file,".ecav")) # eCAVIAR coloc results - `ecav.coloc`
+  # trim `sum_stat` and `ecav.coloc` to remove high r2 clumps
+  # (these are already trimmed from MRLocus `res`)
+  keep <- scan(paste0(dir,"/",file,".mrl_keep"), quiet=TRUE)
+  sum_stat <- sum_stat[keep]
+  ecav.coloc <- ecav.coloc[keep]
+  # any true eSNPs in the clumps
   in.clump <- sapply(sum_stat, function(x) any(x$eqtl.true != 0))
   if (any(in.clump)) {
     mrlocus <- sapply(which(in.clump), function(j) {
