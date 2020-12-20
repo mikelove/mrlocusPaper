09f1039a0396921d074014be7c82b422d3bbbcdd
mikelove <michaelisaiahlove@gmail.com>
2020-12-20 09:26:44 -0500

    update timing

--- sim/timing.R
+++ sim/timing.R
@@ -1,46 +1,69 @@
 myscan <- function(x) as.numeric(scan(x, what="char", quiet=TRUE)[10])
+mylistfiles <- function(x) {
+  c(list.files("bench/1", x, full.names=TRUE),
+    list.files("bench/high_n", x, full.names=TRUE))
+  }
 
-mrl <- list.files("bench","mrlocus", full.names=TRUE)
-mrl.time <- sapply(mrl, myscan)
-hist(log10(mrl.time))
+mrl <- mylistfiles("_mrlocus")
+mrl.time <- sapply(mrl, myscan, USE.NAMES=FALSE)
+length(mrl.time)
+hist(mrl.time)
 
-ptw <- list.files("bench","ptwas", full.names=TRUE)
-ptw.time <- sapply(ptw, myscan)
-hist(log10(ptw.time))
+ecav <- mylistfiles("_ecav.bench")
+ecav.time <- sapply(ecav, myscan, USE.NAMES=FALSE)
+length(ecav.time)
+hist(ecav.time)
 
-twmr <- list.files("bench","twmr", full.names=TRUE)
-twmr.time <- sapply(twmr, myscan)
-hist(log10(twmr.time))
+emrl <- mylistfiles("ecav-mrlocus")
+emrl.time <- sapply(emrl, myscan, USE.NAMES=FALSE)
+length(emrl.time)
+hist(emrl.time)
+
+ptw <- mylistfiles("ptwas")
+ptw.time <- sapply(ptw, myscan, USE.NAMES=FALSE)
+length(ptw.time)
+hist(ptw.time)
+
+twmr <- mylistfiles("twmr")
+twmr.time <- sapply(twmr, myscan, USE.NAMES=FALSE)
+length(twmr.time)
+hist(twmr.time)
 
 #save(twmr.time,ptw.time,mrl.time, file="timing.rda")
 load("timing.rda")
 
 library(ggplot2)
-mths <- c("twmr","ptwas","mrlocus")
-dat <- data.frame(seconds=c(twmr.time,ptw.time,mrl.time),
-                  method=factor(rep(mths,each=240),levels=mths))
+mths <- c("twmr","ptwas","ecaviar","ecaviar-mrlocus","mrlocus")
+dat <- data.frame(seconds=c(twmr.time,ptw.time,
+                            ecav.time,emrl.time,mrl.time),
+                  method=factor(rep(mths,each=80),levels=mths))
 
-pdf(file="../supp/figs/runtime.pdf", width=4, height=4)
+library(dplyr)
+ms <- dat %>% group_by(method) %>%
+  summarize(mean=mean(seconds)) %>%
+  pull(mean)
+
+tab <- data.frame(
+  method=mths,
+  seconds=c(10,10,10,60,300),
+  label=paste("mean:",round(ms,2),"s"))
+
+#pdf(file="../supp/figs/runtime.pdf", width=4, height=4)
 
 ggplot(dat, aes(method,seconds)) +
   geom_boxplot() +
-  scale_y_log10(breaks=c(2,5,10,30,60,120,300,600)) +
+  scale_y_log10(limits=c(1,300), breaks=c(1,2,5,10,30,60,120,300)) +
   ylab("runtime (seconds)") +
   geom_text(aes(method,seconds,label=label),
-            data.frame(method=rep("twmr",4),
-                       seconds=c(60,120,300,600),
-                       label=c("1 min","2 min","5 min","10 min")),
+            data.frame(method=rep("twmr",3),
+                       seconds=c(60,120,300),
+                       label=c("1 min","2 min","5 min")),
             fontface="bold") +
   geom_label(aes(method,seconds,label=label),
              fill="cornsilk",
-             data.frame(
-               method=c("twmr","ptwas","mrlocus"),
-               seconds=c(15,15,300),
-               label=paste("mean:",
-                           round(sapply(list(twmr.time,ptw.time,mrl.time), mean),2),
-                           "s")))
+             tab)
 
-dev.off()
+#dev.off()
 
 mean(mrl.time)
 mean(twmr.time)
